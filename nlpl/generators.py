0034ce4f641d4ff39c48a822e863091fad47ec38
Nemanja Milićević <the.nemanja.milicevic@gmail.com>
2020-03-07 22:15:00 +0100

    Create sample generators module

---
+++ nlpl/generators.py
@@ -0,0 +1,99 @@
+from collections import Generator
+from typing import List, Tuple
+
+import numpy as np
+from keras_preprocessing import sequence
+
+from sampling import Sampler
+
+
+class CBOWSampleGenerator(Generator):
+    """Generates training samples for continous bag of words model training.
+
+    This sample generator also supports negative sampling and batching.
+
+    """
+
+    def __init__(
+        self,
+        sequences: List[List[int]],
+        sampler: Sampler,
+        window_size: int = 5,
+        negatives: int = 2,
+        batch_size: int = 32,
+    ) -> None:
+        self._sequences = sequences
+        self._sampler = sampler
+        self._window_size = window_size
+        self._negatives = negatives
+        self._batch_size = batch_size
+
+        self._reset_batch()
+
+    def throw(self, type=None, value=None, traceback=None) -> None:
+        raise super().throw(type, value, traceback)
+
+    def send(self, _) -> Tuple[List, np.ndarray]:
+        context_size: int = self._window_size * 2
+
+        iteration = 0
+        while True:
+            sentence = self._sequences[iteration]
+            iteration = (iteration + 1) % len(self._sequences)
+
+            for index, word_index in enumerate(sentence):
+                start = index - self._window_size
+                end = index + self._window_size
+
+                context_words: List[int] = []
+                for current_word_idx in range(start, end + 1):
+                    is_label_word = current_word_idx == index
+                    if 0 <= current_word_idx < len(sentence) and not is_label_word:
+                        context_words.append(sentence[current_word_idx])
+
+                self._add_to_batch(
+                    context_words=context_words, target_word=word_index, label=1
+                )
+
+                negative_samples = self._sampler.sample_negatives(
+                    ignore_word_index=word_index, n_samples=self._negatives
+                )
+
+                for negative_word in negative_samples:
+                    self._add_to_batch(
+                        context_words=context_words, target_word=negative_word, label=0
+                    )
+
+                    if self._is_batch_ready():
+                        return self._process_batch(context_size)
+
+    def _is_batch_ready(self) -> bool:
+        return len(self._contexts_batch) >= self._batch_size
+
+    def _reset_batch(self) -> None:
+        self._contexts_batch: List[List[int]] = []
+        self._targets_batch: List[List[int]] = []
+        self._labels_batch: List[int] = []
+
+    def _add_to_batch(
+        self, context_words: List[int], target_word: int, label: int
+    ) -> None:
+        self._contexts_batch.append(context_words)
+        self._targets_batch.append([target_word])
+        self._labels_batch.append(label)
+
+    def _process_batch(self, context_size: int) -> Tuple[List, np.ndarray]:
+        context_sequence = sequence.pad_sequences(
+            self._contexts_batch, maxlen=context_size
+        )
+        target_sequence = np.array(self._targets_batch)
+
+        # Shuffle batch
+        shuffled_index = np.random.permutation(self._batch_size)
+        context_sequence = context_sequence[shuffled_index]
+        target_sequence = target_sequence[shuffled_index]
+        labels = np.array(self._labels_batch)[shuffled_index]
+
+        self._reset_batch()
+
+        return [target_sequence, context_sequence], labels
--- nlpl/train.py
+++ nlpl/train.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import pickle
-from typing import Any, List, Dict, Tuple, Generator
+from typing import List, Dict, Tuple, Iterator
 
 import numpy as np
 import pandas as pd
@@ -9,9 +9,9 @@ from keras.callbacks import TensorBoard, ModelCheckpoint
 from keras.layers import dot, Input, Dense, Reshape
 from keras.layers.embeddings import Embedding
 from keras.models import Model
-from keras_preprocessing import sequence
 from keras_preprocessing.text import Tokenizer
 
+import generators
 from sampling import Sampler
 
 
@@ -28,9 +28,9 @@ NUM_WORDS: int = 50_000 + 1  # +1 for PAD token.
 EMBEDDING_DIM: int = 256
 
 EPOCHS: int = 20
-STEPS_PER_EPOCH: int = 10  # 10_000_000
+STEPS_PER_EPOCH: int = 10_000_000
 
-TENSORBOARD_LOGS_DIR = os.path.join(ROOT_DIR, ".tensorboard_logs")
+TENSORBOARD_LOGS_DIR: str = os.path.join(ROOT_DIR, ".tensorboard_logs")
 
 
 def load_sampler_and_sequences(sentences_path: str) -> Tuple[Sampler, List[List[int]]]:
@@ -101,66 +101,6 @@ def create_tokenizer(
     return tokenizer
 
 
-def generate_training_samples(
-    sequences: List[List[int]],
-    sampler: Sampler,
-    window_size: int = 5,
-    negatives: int = 2,
-    batch_size: int = 32,
-) -> Generator[Tuple[List[Any], np.ndarray], None, None]:
-    context_size: int = window_size * 2
-
-    contexts_batch = []
-    targets_batch = []
-    labels_batch = []
-
-    iteration = 0
-    while True:
-        sentence = sequences[iteration]
-        iteration = (iteration + 1) % len(sequences)
-
-        for index, word_index in enumerate(sentence):
-            start = index - window_size
-            end = index + window_size
-
-            context_words: List[int] = []
-            for current_word_idx in range(start, end + 1):
-                is_label_word = current_word_idx == index
-                if 0 <= current_word_idx < len(sentence) and not is_label_word:
-                    context_words.append(sentence[current_word_idx])
-
-            contexts_batch.append(context_words)
-            targets_batch.append([word_index])
-            labels_batch.append(1)
-
-            negative_samples = sampler.sample_negatives(
-                ignore_word_index=word_index, n_samples=negatives
-            )
-
-            for negative_word in negative_samples:
-                contexts_batch.append(context_words)
-                targets_batch.append([negative_word])
-                labels_batch.append(0)
-
-                if len(contexts_batch) >= batch_size:
-                    context_sequence = sequence.pad_sequences(
-                        contexts_batch, maxlen=context_size
-                    )
-                    target_sequence = np.array(targets_batch)
-
-                    # Shuffle batch
-                    shuffled_index = np.random.permutation(batch_size)
-                    context_sequence = context_sequence[shuffled_index]
-                    target_sequence = target_sequence[shuffled_index]
-                    labels = np.array(labels_batch)[shuffled_index]
-
-                    contexts_batch = []
-                    targets_batch = []
-                    labels_batch = []
-
-                    yield [target_sequence, context_sequence], labels
-
-
 def compile_model(context_size: int, target_size: int = 1) -> Model:
     input_target = Input((target_size,))
     input_context = Input((context_size,))
@@ -195,14 +135,19 @@ def compile_model(context_size: int, target_size: int = 1) -> Model:
 
 
 if __name__ == "__main__":
-    sentences_path: str = os.path.join(DATA_DIR, "processed", "sentences.txt")
+    sentences_path = os.path.join(DATA_DIR, "processed", "sentences.txt")
     sampler, sequences = load_sampler_and_sequences(sentences_path)
 
-    window_size = 5
+    window_size: int = 5
 
-    model = compile_model(context_size=window_size * 2)
+    model: Model = compile_model(context_size=window_size * 2)
+
+    sample_generator: Iterator = generators.CBOWSampleGenerator(  # type: ignore
+        sequences, sampler, window_size=window_size
+    )
+
     model.fit_generator(
-        generate_training_samples(sequences, sampler, window_size=window_size),
+        sample_generator,
         steps_per_epoch=STEPS_PER_EPOCH,
         epochs=EPOCHS,
         callbacks=[
